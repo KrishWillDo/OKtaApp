642816c9817d96d1192b3eb2f4230e2e73e3f0ac
Krishna Kanth <krishnovatiwe@gmail.com>
2024-11-11 10:27:45 +0530

    Added name to home page.

--- OKtaApp/Controllers/HomeController.cs
+++ OKtaApp/Controllers/HomeController.cs
@@ -5,6 +5,10 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
+        if (User.Identity.IsAuthenticated)
+        {
+            ViewBag.UserDetail = User.Claims.FirstOrDefault(x => x.Type == "name").Value;
+        }
         return View();
     }
 
